ce4f70a77806fd883f442684c02f53e077b9127b
anhlnp <145736156+anhlnp@users.noreply.github.com>
2024-10-07 07:46:05 +0700

    Update index.js update cross origin fix

--- BE/src/index.js
+++ BE/src/index.js
@@ -10,28 +10,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(function (req, res, next) {
-// Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_SERVER_URL);
-// Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
-// Pass to next layer of middleware
-next();
-    
-});
+app.use(cors({
+  origin: process.env.REACT_APP_SERVER_URL, // Set this to your frontend URL
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization'],
+  credentials: true, // Include credentials if necessary
+}));
 
-app.use(cors(
-//     {
-// origin: process.env.REACT_APP_SERVER_URL,
-// methods: ['GET', 'POST', 'PUT'],
-// allowedHeaders: ['Content-Type', 'Authorization'],
-// }
-));
 app.use(bodyParser.json());
 
 app.use('/quizzes', quizRoutes);

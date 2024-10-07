8731c65a9803d98241f02ce14af82e024bf463c7
anhlnp <anhphucpro13@gmail.com>
2024-10-07 08:09:14 +0700

    fix index.js

--- BE/src/index.js
+++ BE/src/index.js
@@ -7,9 +7,25 @@ const quizRoutes = require('./resources/routes/quizzes');
 const questionRoutes = require('./resources/routes/question');
 
 const app = express();
+const allowedOrigins = [process.env.REACT_APP_SERVER_URL];
+
 app.use(cors({
-    origin: process.env.REACT_APP_SERVER_URL,
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin) return callback(null, true);
+
+    // Check if the origin is in the allowedOrigins array
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Origin is allowed
+    } else {
+      callback(new Error('Not allowed by CORS')); // Origin is not allowed
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization'],
+  credentials: true,
 }));
+
 app.use(bodyParser.json());
 
 app.use('/quizzes', quizRoutes);
@@ -25,7 +41,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 
 app.listen(port, () => {
--- FE/src/services/api.js
+++ FE/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_URL = 'https://vii-sdn-ass.onrender.com';
 
-const API_URL = process.env.REACT_APP_API_URL || 3000; // Replace with your server URL
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000'; // Replace with your server URL
 
 // Quizzes API
 export const getQuizzes = () => axios.get(`${API_URL}/quizzes`);

fedff40577a4805c075278893abff1379856c518
anhlnp <145736156+anhlnp@users.noreply.github.com>
2024-10-07 07:50:15 +0700

    Update index.js update cross origin fix

--- BE/src/index.js
+++ BE/src/index.js
@@ -10,12 +10,20 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.REACT_APP_SERVER_URL, // Set this to your frontend URL
-  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization'],
-  credentials: true, // Include credentials if necessary
-}));
+// app.use(cors({
+//   origin: process.env.REACT_APP_SERVER_URL, // Set this to your frontend URL
+//   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+//   allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization'],
+//   credentials: true, // Include credentials if necessary
+// }));
+
+app.use(function (req, res, next) {
+res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_SERVER_URL);
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+res.setHeader('Access-Control-Allow-Credentials', true);
+next();
+})
 
 app.use(bodyParser.json());
 
